957067089b931c0eb9fd1a0d37717142c988c480
Branko Sulovic <44022084+Salcoman@users.noreply.github.com>
2023-06-03 14:13:22 +0200

    GameManagerInit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private string[] sastojci = new string[] { "cokolada","vanila","banana","pistaci","jagoda"};
+    private List<string[]> narudzbina = new List<string[]>();
+
     // Start is called before the first frame update
     void Start()
     {
+        generisiRecepte();
+        foreach (string[] s in narudzbina)
+        {
+            foreach (string element in s)
+            {
+                Debug.Log(element);
+            }
+        }
         
+    }
+
+    public void generisiRecepte()
+    {
+        string[] recept = new string[3];
+        for (int i = 0; i < 3; i++)
+        {
+            recept[i] = sastojci[Random.Range(0, sastojci.Length)];
+        }
+
+        narudzbina.Add(recept);
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -31,12 +31,6 @@ public class PlayerManager : MonoBehaviour
             gameObject.transform.forward = move;
         }
 
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
