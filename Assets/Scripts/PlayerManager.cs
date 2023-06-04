3348d2234693131f83bbf9293a20c86d4260b2b1
Branko Sulovic <44022084+Salcoman@users.noreply.github.com>
2023-06-04 06:58:14 +0200

    tanjir skoro spreman

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,11 +6,22 @@ public class PlayerManager : MonoBehaviour
 {
     private Vector3 scaleChange;
     private bool plateCollected;
+    private bool entered;
+    private string state;
     public GameObject _goPlate;
+
+    public List<Sprite> cakeSprites = new List<Sprite>();
+    public List<string> fishOrder = new List<string>();
+    public GameObject _goFishOrder;
+    private List<GameObject> fishOrderImages = new List<GameObject>();
 
     private void Start()
     {
-        scaleChange = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+        foreach (Transform child in _goFishOrder.transform)
+        {
+            fishOrderImages.Add(child.gameObject);
+            child.gameObject.SetActive(false);
+        }
     }
     private float movementSpeed = 50f;
 
@@ -29,15 +40,81 @@ public class PlayerManager : MonoBehaviour
             this.transform.localScale = new Vector3(-1*Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
 
         }
-        if (Input.GetButtonDown("E"))
+        if (Input.GetButtonDown("E") && entered)
         {
-
+            switch (state)
+            {
+                case "Plate":
+                    togglePlate(true);
+                    break;
+                case "Banana":
+                    togglePlate(true);
+                    break;
+                case "Kiwi":
+                    togglePlate(true);
+                    break;
+                case "Jagoda":
+                    togglePlate(true);
+                    break;
+                case "Cokolada":
+                    togglePlate(true);
+                    break;
+                case "Borovnica":
+                    togglePlate(true);
+                    break;
+                case "Recycle":
+                    togglePlate(false);
+                    break;
+                default:
+                    break;
+            }
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(collision.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        if (collision.tag.Equals("Plate"))
+        {
+            state = "Plate";
+        }
+        if (collision.tag.Equals("Recycle"))
+        {
+            state = "Recycle";
+        }
+        if (collision.tag.Equals("Kiwi"))
+        {
+            state = "Kiwi";
+        }
+        if (collision.tag.Equals("Jagoda"))
+        {
+            state = "Jagoda";
+        }
+        if (collision.tag.Equals("Cokolada"))
+        {
+            state = "Cokolada";
+        }
+        if (collision.tag.Equals("Borovnica"))
+        {
+            state = "Borovnica";
+        }
+        if (collision.tag.Equals("Banana"))
+        {
+            state = "Banana";
+        }
+
+        entered = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        entered = false;
     }
 
     public void togglePlate(bool boolVar)
     {
         plateCollected = boolVar;
-
+        _goPlate.SetActive(plateCollected);
+        
     }
 }
